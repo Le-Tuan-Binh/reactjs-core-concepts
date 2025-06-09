ae790bde42a014a0df9a48446e0d1fbdabc6f2dd
tbin <TBinTB0003@gmail.com>
2025-06-10 03:51:44 +0700

    Props in react

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,28 @@ import "./App.css";
 import { Avatar } from "./components/Avatar";
 
 function App() {
-	const name = "tbin";
-	const handleGetFullYear = () => {
-		return new Date().getFullYear();
-	};
 	return (
 		<>
-			<div className="content">
-				<Avatar />
-				<p>My name is {name}</p>
-				<p>This year is {handleGetFullYear()}</p>
+			<div
+				className="content"
+				style={{
+					display: "flex",
+					gap: "20px",
+					alignItems: "center",
+				}}
+			>
+				<Avatar
+					image_url="https://images.unsplash.com/photo-1748939832727-a2f0abdff540?q=80&w=1528&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+					size={100}
+				/>
+				<Avatar
+					image_url="https://images.unsplash.com/photo-1748939832727-a2f0abdff540?q=80&w=1528&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+					size={200}
+				/>
+				<Avatar
+					image_url="https://images.unsplash.com/photo-1748939832727-a2f0abdff540?q=80&w=1528&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+					size={100}
+				/>
 			</div>
 		</>
 	);
--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,16 +1,19 @@
-const image_url =
-	"https://images.unsplash.com/photo-1748939832727-a2f0abdff540?q=80&w=1528&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+interface AvatarProps {
+	image_url: string;
+	size: number;
+}
 
 const handleClickImage = () => {
 	console.log("Click Me");
 };
-export function Avatar() {
+export function Avatar(props: AvatarProps) {
+	const { image_url, size } = props;
 	return (
 		<img
 			src={image_url}
 			style={{
-				width: "120px",
-				height: "120px",
+				width: size,
+				height: size,
 				borderRadius: "100px",
 				objectFit: "cover",
 			}}
